8199c5bb54be95fbccf4f08351ab4ea4f0d64e29
gzf <gzf.dev@gmail.com>
2026-06-03 14:01:46 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  // Do NOT set basePath or assetPrefix unless you know what you're doing
 };
 
 module.exports = nextConfig;
