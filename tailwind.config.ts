7e3654fe12c2667a099fe0cf60c443dac9a68472
gzf <gzf.dev@gmail.com>
2026-06-03 13:28:56 +0330

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,21 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class", // ✅ enable class-based dark mode
+  darkMode: 'media', // use system preference
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-bg': '#0C0E13',
+        'grad-start': '#ffffff',
+        'grad-mid': '#CAE0FF',
+        'grad-end': '#6597E9',
+      },
+    },
   },
   plugins: [],
 };
